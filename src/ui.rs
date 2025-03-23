c278b8e93f029392d94e86ca7730defaac474590
think <thinkcodestudio@qq.com>
2025-03-23 11:52:42 +0800

    feat: add enable widget

--- src/ui.rs
+++ src/ui.rs
@@ -13,6 +13,7 @@ pub struct MainUi {
     selected_data_bits: DataBits,
     selected_stop_bits: StopBits,
     selected_parity: Parity,
+    en_connect: bool,
 }
 
 impl Default for MainUi {
@@ -29,6 +30,7 @@ impl Default for MainUi {
             selected_data_bits: DataBits::Eight,
             selected_stop_bits: StopBits::One,
             selected_parity: Parity::None,
+            en_connect: true,
         }
     }
 }
@@ -39,90 +41,111 @@ impl eframe::App for MainUi {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.horizontal(|ui| {
-                egui::ComboBox::from_label("Port")
-                    .selected_text(format!("{}", self.selected_port.port_name))
-                    .show_ui(ui, |ui| {
-                        for port in &self.serial_list {
-                            ui.selectable_value::<SerialPortInfo>(
-                                &mut self.selected_port,
-                                port.clone(),
-                                port.port_name.clone(),
-                            );
-                        }
-                    });
+                if ui
+                    .add_enabled(self.en_connect, egui::Button::new("🔄"))
+                    .clicked()
+                {
+                    self.serial_list = available_ports().unwrap_or(vec![])
+                }
 
-                egui::ComboBox::from_label("Baud")
-                    .selected_text(format!("{}", self.selected_baud))
-                    .show_ui(ui, |ui| {
-                        for baud in &self.baud_list {
+                ui.add_enabled_ui(self.en_connect, |ui| {
+                    egui::ComboBox::from_label("Port")
+                        .selected_text(format!("{}", self.selected_port.port_name))
+                        .show_ui(ui, |ui| {
+                            for port in &self.serial_list {
+                                ui.selectable_value::<SerialPortInfo>(
+                                    &mut self.selected_port,
+                                    port.clone(),
+                                    port.port_name.clone(),
+                                );
+                            }
+                        });
+                });
+
+                ui.add_enabled_ui(self.en_connect, |ui| {
+                    egui::ComboBox::from_label("Baud")
+                        .selected_text(format!("{}", self.selected_baud))
+                        .show_ui(ui, |ui| {
+                            for baud in &self.baud_list {
+                                ui.selectable_value(
+                                    &mut self.selected_baud,
+                                    baud.clone(),
+                                    baud.to_string(),
+                                );
+                            }
+                        });
+                });
+
+                ui.add_enabled_ui(self.en_connect, |ui| {
+                    egui::ComboBox::from_label("Data Bits")
+                        .selected_text(format!("{}", u8::from(self.selected_data_bits)))
+                        .show_ui(ui, |ui| {
                             ui.selectable_value(
-                                &mut self.selected_baud,
-                                baud.clone(),
-                                baud.to_string(),
+                                &mut self.selected_data_bits,
+                                DataBits::Five,
+                                format!("{}", u8::from(DataBits::Five)),
                             );
-                        }
-                    });
+                            ui.selectable_value(
+                                &mut self.selected_data_bits,
+                                DataBits::Six,
+                                format!("{}", u8::from(DataBits::Six)),
+                            );
+                            ui.selectable_value(
+                                &mut self.selected_data_bits,
+                                DataBits::Seven,
+                                format!("{}", u8::from(DataBits::Seven)),
+                            );
+                            ui.selectable_value(
+                                &mut self.selected_data_bits,
+                                DataBits::Eight,
+                                format!("{}", u8::from(DataBits::Eight)),
+                            );
+                        });
+                });
 
-                egui::ComboBox::from_label("Data Bits")
-                    .selected_text(format!("{}", u8::from(self.selected_data_bits)))
-                    .show_ui(ui, |ui| {
-                        ui.selectable_value(
-                            &mut self.selected_data_bits,
-                            DataBits::Five,
-                            format!("{}", u8::from(DataBits::Five)),
-                        );
-                        ui.selectable_value(
-                            &mut self.selected_data_bits,
-                            DataBits::Six,
-                            format!("{}", u8::from(DataBits::Six)),
-                        );
-                        ui.selectable_value(
-                            &mut self.selected_data_bits,
-                            DataBits::Seven,
-                            format!("{}", u8::from(DataBits::Seven)),
-                        );
-                        ui.selectable_value(
-                            &mut self.selected_data_bits,
-                            DataBits::Eight,
-                            format!("{}", u8::from(DataBits::Eight)),
-                        );
-                    });
+                ui.add_enabled_ui(self.en_connect, |ui| {
+                    egui::ComboBox::from_label("Stop Bits")
+                        .selected_text(format!("{}", u8::from(self.selected_stop_bits)))
+                        .show_ui(ui, |ui| {
+                            ui.selectable_value(
+                                &mut self.selected_stop_bits,
+                                StopBits::One,
+                                format!("{}", u8::from(StopBits::One)),
+                            );
 
-                egui::ComboBox::from_label("Stop Bits")
-                    .selected_text(format!("{}", u8::from(self.selected_stop_bits)))
-                    .show_ui(ui, |ui| {
-                        ui.selectable_value(
-                            &mut self.selected_stop_bits,
-                            StopBits::One,
-                            format!("{}", u8::from(StopBits::One)),
-                        );
+                            ui.selectable_value(
+                                &mut self.selected_stop_bits,
+                                StopBits::Two,
+                                format!("{}", u8::from(StopBits::Two)),
+                            );
+                        });
+                });
 
-                        ui.selectable_value(
-                            &mut self.selected_stop_bits,
-                            StopBits::Two,
-                            format!("{}", u8::from(StopBits::Two)),
-                        );
-                    });
+                ui.add_enabled_ui(self.en_connect, |ui| {
+                    egui::ComboBox::from_label("Parity")
+                        .selected_text(format!("{}", self.selected_parity))
+                        .show_ui(ui, |ui| {
+                            ui.selectable_value(
+                                &mut self.selected_parity,
+                                Parity::None,
+                                format!("{}", Parity::None),
+                            );
+                            ui.selectable_value(
+                                &mut self.selected_parity,
+                                Parity::Even,
+                                format!("{}", Parity::Even),
+                            );
+                            ui.selectable_value(
+                                &mut self.selected_parity,
+                                Parity::Odd,
+                                format!("{}", Parity::Odd),
+                            );
+                        });
+                });
 
-                egui::ComboBox::from_label("Parity")
-                    .selected_text(format!("{}", self.selected_parity))
-                    .show_ui(ui, |ui| {
-                        ui.selectable_value(
-                            &mut self.selected_parity,
-                            Parity::None,
-                            format!("{}", Parity::None),
-                        );
-                        ui.selectable_value(
-                            &mut self.selected_parity,
-                            Parity::Even,
-                            format!("{}", Parity::Even),
-                        );
-                        ui.selectable_value(
-                            &mut self.selected_parity,
-                            Parity::Odd,
-                            format!("{}", Parity::Odd),
-                        );
-                    });
+                if ui.add(egui::Button::new("connection")).clicked() {
+                    self.serial_list = available_ports().unwrap_or(vec![])
+                }
             });
         });
     }
